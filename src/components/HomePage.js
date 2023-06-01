ac73ef06e9681147ea2a51f577f3752a6bbae881
clodewerks <clodewerks@gmail.com>
2023-06-01 15:42:20 -0700

    Update for new year!

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -8,12 +8,11 @@ function HomePage() {
       </div>
       <div className="container">
         <div className="container center">
-          <h2>2022 Festival</h2>
+          <h2>2023 Festival</h2>
         </div>
         <div className="container-fluid"><div className="row">
           <div className="col-sm center">
-            <p>Watch it back here: </p>
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLQ-siEB7QOARHPQ7ZLucDFBuQE1dN2e1A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <p>Save the date: August 19th, Portland Oregon!!</p>
           </div>
         </div>
         </div>
