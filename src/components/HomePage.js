a4f97ff3d314f2a1b313723f3000fcb023a68b6f
clodewerks <clodewerks@gmail.com>
2024-08-14 16:16:54 -0700

    Adding lineups to home HomePage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -12,13 +12,17 @@ function HomePage() {
         <p><strong>Saturday, Sept 21, 2024</strong> <br/>
         4:30-10 pm<br/>
         <strong>Shaku Bar</strong><br/>
-        3448 NE Sandy Blvd., Portland
+        3448 NE Sandy Blvd., Portland<br/><br/>
+        <strong>Featuring:</strong><br/>
+        Exosphere Project, Oleada, Jake Soffer, Paul Summers, The Other Place, Lambent
         </p>
         <h2>Day Two:</h2>
         <strong>Sunday, Sept 22, 2024</strong><br/>
         2:00 - 5 pm<br/>
         <strong>Stage 9</strong><br/>
-        216 N Tillamook St., Portland
+        216 N Tillamook St., Portland<br/><br/>
+        <strong>Featuring:</strong><br/>
+        Clodewerks, Twenty Three Suns, Transparent Aluminum, corey emil
         </div>
 
       </div>
