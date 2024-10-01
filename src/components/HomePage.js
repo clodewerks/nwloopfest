7cd79290f1e96555c28e15e673712334eb7f4002
clodewerks <clodewerks@gmail.com>
2024-10-01 13:27:08 -0700

    Reset homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -8,21 +8,8 @@ function HomePage() {
       </div>
       <div className="container">
         <div className="container center">
-        <h1>Day One:</h1>
-        <p><strong>Saturday, Sept 21, 2024</strong> <br/>
-        4:30-10 pm<br/>
-        <strong>Shaku Bar</strong><br/>
-        3448 NE Sandy Blvd., Portland<br/><br/>
-        <strong>Featuring:</strong><br/>
-        Exosphere Project, Oleada, Jake Soffer, Paul Summers, The Other Place, Lambent
-        </p>
-        <h2>Day Two:</h2>
-        <strong>Sunday, Sept 22, 2024</strong><br/>
-        2:00 - 5 pm<br/>
-        <strong>Stage 9</strong><br/>
-        216 N Tillamook St., Portland<br/><br/>
-        <strong>Featuring:</strong><br/>
-        Clodewerks, Twenty Three Suns, Transparent Aluminum, corey emil
+        <h2>See you in 2025!</h2>
+          <p>In the meantime watch some of the <a href="fests">previous years' footage</a>.</p>
         </div>
 
       </div>
