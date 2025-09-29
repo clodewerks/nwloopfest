f85ade967fa745d49309f83e00d55ae773f4e417
Kyle Linneman <klinneman@navexglobal.com>
2025-09-29 13:29:24 -0700

    Adding lineup

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -12,6 +12,7 @@ function HomePage() {
         <h3>Synth Library Portland</h3>
         <h4>$10-$20 Sliding Scale</h4>
         <h4>All Ages</h4>
+        <h2>Featuring: Clodewerks, Exosphere Project, -2, Shifting harbor, Jensmith Marx, Gnostic Evolution, Shadow Tapes, Twenty Three Suns, Jake Soffer, Bob Amstadt</h2>
         <p><a href="https://withfriends.events/event/78wwJexj/nw-loopfest/"  >Get Tickets here!</a></p>
         </div>
 
