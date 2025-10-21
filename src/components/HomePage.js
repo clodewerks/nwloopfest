aaeb8efb2f394826f58248273c69561bb0c4cc81
Kyle Linneman <klinneman@navexglobal.com>
2025-10-21 10:56:46 -0700

    Post Event update

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -8,12 +8,11 @@ function HomePage() {
       </div>
       <div className="container">
         <div className="container center">
-        <h2>October 18th, 2025: 1PM-8PM</h2>
-        <h3>Synth Library Portland</h3>
-        <h4>$10-$20 Sliding Scale</h4>
-        <h4>All Ages</h4>
-        <h2>Featuring: Clodewerks, Exosphere Project, -2, Shifting harbor, Jensmith Marx, Gnostic Evolution, Shadow Tapes, Twenty Three Suns, Jake Soffer, Bob Amstadt</h2>
-        <p><a href="https://withfriends.events/event/78wwJexj/nw-loopfest/"  >Get Tickets here!</a></p>
+        <h2>See you next year!</h2>
+        <p>
+          Thank you for making NW Loopfest 2025 a huge success! We can't wait
+          to see you again next year for more fun, music, and community spirit.
+        </p>
         </div>
 
       </div>
